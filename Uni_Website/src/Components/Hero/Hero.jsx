af8bf03f1a2f5f647ce80f8f2bd2305accef3b8c
Craig Rosario <craig.rosario04@gmail.com>
2024-12-04 20:42:46 +0530

    added css

--- Uni_Website/src/Components/Hero/Hero.jsx
+++ Uni_Website/src/Components/Hero/Hero.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+
 const Hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero contianer'>
+      <div className='heroText'>
+        <h1>
+            WELCOME TO YOUR FUTURE
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque tenetur sed voluptatum necessitatibus recusandae! Cumque qui repudiandae nam eos modi. Inventore vel hic facilis provident rerum dolor aspernatur officiis! Deserunt!</p>
+        <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+      </div>
     </div>
   )
 }
